eda334558f347aa08d9e00857aa1902ebcd40ef7
Arcturus <leehyobeen747@gmail.com>
2025-07-22 03:25:30 +0900

    [#10] feat: 카드 플립 효과 구현

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
-      "./app/**/*.{js,ts,jsx,tsx}" // Next.js 프로젝트면 포함
+      "./app/**/*.{js,ts,jsx,tsx}" 
     ],
     theme: {
       extend: {

49cfc4cb7a4dbc77e79e28386b5d2d3b47e751f1
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-08-02 15:46:37 +0900

    [#17] chore: 지원분야 컴포넌트 -> SelectableButtonGroup 공용 컴포넌트로 변경

--- app/project-apply/page.tsx
+++ app/project-apply/page.tsx
@@ -1,5 +1,5 @@
 import { projectApplyUserData } from "@/mocks/projectApplyUserData";
-import ApplyFieldSelector from "./components/ApplyFieldSelector";
+import SelectableButtonGroup from "../../components/SelectableButtonGroup";
 import AvailableDaysSelector from "./components/AvailableDaysSelector";
 import OfflineAvailabilityToggle from "./components/OfflineAvailabilityToggle";
 import SelfIntroductionInput from "./components/SelfIntroductionInput";
@@ -7,11 +7,12 @@ import UserProfileSummary from "./components/UserProfileSummary";
 import WeeklyHourInput from "./components/WeeklyHourInput";
 
 export default function Page() {
+  const partList = ["프론트", "백", "디자인", "기획", "마케팅"];
   return (
     <div className="w-[420px] m-auto flex flex-col py-10">
       <b className="text-[26px] mb-10">프로젝트 지원</b>
       <div className="flex flex-col gap-15">
-        <ApplyFieldSelector />
+        <SelectableButtonGroup title={"지원 분야"} optionList={partList} />
         <SelfIntroductionInput />
         <WeeklyHourInput />
         <AvailableDaysSelector />

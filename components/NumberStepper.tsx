335c8fe11d87c035757d10f3e369db801f6d0632
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-08-02 16:39:25 +0900

    [#17] chore: 주당 투자가능 시간 컴포넌트 -> NumberStepper 컴포넌트화

--- components/NumberStepper.tsx
+++ components/NumberStepper.tsx
@@ -2,37 +2,52 @@
 
 import { useState } from "react";
 
-function WeeklyHourInput() {
-  const [hour, setHour] = useState(1); // 0 아래로는 안내려가게 , 7 * 24 = 168 0<= hour <= 168
-  const [overHour, setOverHour] = useState(false); //
+export default function NumberStepper({
+  title,
+  initValue,
+  min,
+  max,
+  warningMessage,
+}: {
+  title: string;
+  initValue: number;
+  min: number;
+  max: number;
+  warningMessage: string;
+}) {
+  const [number, setNumber] = useState(initValue);
+  const [overNumber, setOverNumber] = useState(false); //
 
   function handleClickPlusButton() {
     // + 버튼 이벤트 핸들러
-    if (hour < 168) {
-      setHour((prev) => (prev += 1));
+    if (number < max) {
+      setNumber((prev) => (prev += 1));
+    } else {
+      setOverNumber(true);
     }
   }
   function handleClickMinusButton() {
     // - 버튼 이벤트 핸들러
-    if (hour > 0) {
-      setHour((prev) => (prev -= 1));
+    if (number > min) {
+      setNumber((prev) => (prev -= 1));
+    } else {
+      setOverNumber(true);
     }
   }
 
-  function handleInputHourChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleInputNumberChange(e: React.ChangeEvent<HTMLInputElement>) {
     const value = e.target.value;
-    if (parseInt(value) <= 168 && parseInt(value) >= 0) {
-      // 1주는 168시간이니 168 최대
-      setHour(parseInt(value));
+    if (parseInt(value) <= max && parseInt(value) >= min) {
+      setNumber(parseInt(value));
     }
     // 백스페이스로 값 지웠을 때 NaN이 되는 것을 방지해서 0으로 만듦
-    else if (value === "") setHour(0);
-    // 숫자가 0과 168 사이가 아닐 때 경고 메세지 띄워주기 -> 한 번 뜨면 안 사자리게 해놨음
-    else setOverHour(true);
+    else if (value === "") setNumber(0);
+    // 숫자가 min과 max 사이가 아닐 때 경고 메세지 띄워주기 -> 한 번 뜨면 안 사자리게 해놨음
+    else setOverNumber(true);
   }
   return (
     <div className="flex flex-col gap-4">
-      <b>주당 투자 가능 시간</b>
+      <b>{title}</b>
       <div className="flex items-center border-collapse">
         <button
           onClick={handleClickMinusButton}
@@ -41,8 +56,8 @@ function WeeklyHourInput() {
           -
         </button>
         <input
-          value={hour}
-          onChange={handleInputHourChange}
+          value={number}
+          onChange={handleInputNumberChange}
           className="w-[40px] h-[40px] box-border border border-[#6BB4FF] text-center outline-0 bg-[#EDF6FF]"
         />
         <button
@@ -53,12 +68,10 @@ function WeeklyHourInput() {
         </button>
       </div>
       <span
-        className={`text-[12px] text-[#6BB4FF] ${overHour ? "" : "hidden"}`}
+        className={`text-[12px] text-[#6BB4FF] ${overNumber ? "" : "hidden"}`}
       >
-        시간은 0시간 이상 168시간 이하로 입력해주세요!
+        {warningMessage}
       </span>
     </div>
   );
 }
-
-export default WeeklyHourInput;

d86edcc75a04829b02a0cd9f1c8bb548d81ed215
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-08-02 16:40:10 +0900

    [#17] feat: 현재 선택된 요일만 확인 가능한 배열 생성

--- app/project-apply/components/AvailableDaysSelector.tsx
+++ app/project-apply/components/AvailableDaysSelector.tsx
@@ -1,19 +1,25 @@
 "use client";
 import ToggleButton from "@/components/ToggleButton";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // 지원분야랑 투자 가능 요일 Toggle Button 사용법은 동일한데, 여기서는 Record 안쓰고 배열 2개 매핑하는 걸로함
 // 요일 개수는 변하지 않으니까 배열로 해도 될 것 같다는 생각..
 
 function AvailableDaysSelector() {
-  const day = ["월", "화", "수", "목", "금", "토", "일"];
-  // 7칸 짜리 배열로 각 요일별로 선택됐는지 안됐는지 확인함
-  const [daySelected, setDaySelected] = useState<boolean[]>(
+  const day = ["월", "화", "수", "목", "금", "토", "일"]; // 요일 담는 배열
+  const [daySelection, setDaySelection] = useState<boolean[]>(
     new Array(7).fill(false)
-  );
-  // 클릭했을 때 각 요일 버튼 상태 바꾸는 함수 -> index로 접근함
+  ); // day 배열과 매핑될 배열, daySelection[0] = false의 의미는 월요일이 현재 선택되지 않았음을 의미
+
+  const [selectedDay, setSelectedDay] = useState<string[]>([]); // true인 요일만 필터링 -> data 보낼 용도
+  // console.log("현재 선택된 요일 : ", selectedDay);
+
+  useEffect(() => {
+    setSelectedDay(day.filter((_, idx) => daySelection[idx] === true));
+  }, [daySelection]);
+
   function handleClickButton(idx: number) {
-    setDaySelected((prev) => {
+    setDaySelection((prev) => {
       const updated = [...prev];
       updated[idx] = !updated[idx];
       return updated;
@@ -29,7 +35,7 @@ function AvailableDaysSelector() {
             content={day}
             width={45}
             height={41}
-            isSelected={daySelected[idx]}
+            isSelected={daySelection[idx]}
             onClick={() => handleClickButton(idx)}
           />
         ))}

847e658a3cc394336d9b72915ad88f0cc5125d6d
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-08-06 17:20:07 +0900

    [#17] feat: 수정하기 버튼 별개 컴포넌트로 분리, 기본정보 컴포넌트 클라이언트 -> 서버 컴포넌트로 변경

--- app/project-apply/components/UserProfileSummary.tsx
+++ app/project-apply/components/UserProfileSummary.tsx
@@ -1,7 +1,5 @@
-"use client"; // onClick 하나때문에 일단 클라이언트 컴포넌트로 만듦
 import { userProfile } from "@/types/userProfile";
-import modifyButton from "@/public/images/right_arrow_icon.svg";
-import Image from "next/image";
+import ModifyButton from "./ModifyButton";
 
 export default function UserProfileSummary({
   name = "이름",
@@ -14,13 +12,7 @@ export default function UserProfileSummary({
     <div className="flex flex-col gap-5">
       <span className="flex justify-between items-center ">
         <b>기본 정보</b>
-        <span
-          onClick={() => alert("기본정보 수정")}
-          className="text-[#AD5FFF] flex gap-2 cursor-pointer text-[14px]"
-        >
-          수정하기
-          <Image src={modifyButton} alt="수정하기" width={8} height={12} />
-        </span>
+        <ModifyButton />
       </span>
       <hr className="border-4 border-[#F8F8F8]" />
       <div className="flex flex-col gap-6">

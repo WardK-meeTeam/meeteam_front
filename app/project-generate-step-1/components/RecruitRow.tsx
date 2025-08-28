19934c3879626a7083ccfa018e72bfcb55614a57
unknown <jhje5595@naver.com>
2025-08-29 00:44:49 +0900

    [#26] feat: FieldSelector 컴포넌트 적용

--- app/project-generate-step-1/components/RecruitRow.tsx
+++ app/project-generate-step-1/components/RecruitRow.tsx
@@ -1,22 +1,36 @@
 "use client";
-import Selectable from "@/components/Selectable";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import FieldSelector from "./FieldSelector";
+import { recruitFieldItem } from "@/store/projectGenerateStore";
 
-const options = ["iOS", "Android", "Web"];
+// 일단 최소인원 1명, 최대인원 9명으로 해둠
 
-export default function RecruitRow() {
+export default function RecruitRow({
+  onFieldChange,
+}: {
+  onFieldChange: (payload: recruitFieldItem) => void;
+}) {
+  const [field, setField] = useState<string | null>(null);
   const [number, setNumber] = useState<number>(1);
-  const onClickButton = (e) => {
-    if (e.target.value === "+") {
+  const onClickButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (e.currentTarget.value === "+" && number < 9) {
       setNumber((prev) => prev + 1);
-    } else if (e.target.value === "-") {
+    } else if (e.currentTarget.value === "-" && number > 1) {
       setNumber((prev) => prev - 1);
     }
   };
+
+  useEffect(() => {
+    if (field !== null) {
+      onFieldChange({
+        field: field,
+        numOfPeople: number,
+      });
+    }
+  }, [field, number]);
   return (
     <div className="w-full flex flex-row flex-1 gap-2 items-center">
-      <Selectable options={options} />
-      <Selectable options={options} />
+      <FieldSelector onChangeOptions={setField} />
       <div className="flex items-center">
         <button
           className="w-[40px] h-[40px] box-border flex justify-center items-center cursor-pointer"

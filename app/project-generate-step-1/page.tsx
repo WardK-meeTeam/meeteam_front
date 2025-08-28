8ccbf5721d7eeadf48c8b37b8b6751f39e3493bf
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-08-28 17:49:33 +0900

    [#26] feat: 프로젝트 생성시 필요한 정보 State로 관리

--- app/project-generate-step-1/page.tsx
+++ app/project-generate-step-1/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import BinaryOptionSelector from "@/components/BinaryOptionSelector";
-import Selectable from "@/components/Selectable";
 import SelectableButtonGroup from "@/components/SelectableButtonGroup";
 import ImageSelector from "./components/ImageSelector";
 import Recruit from "./components/Recruit";
@@ -10,6 +9,9 @@ import MainButton from "@/components/MainButton";
 import SubButton from "@/components/SubButton";
 import { useRouter } from "next/navigation";
 import Input from "./components/Input";
+import { useEffect } from "react";
+import { useProjectGenerateStore } from "@/store/projectGenerateStore";
+import FieldSelector from "./components/FieldSelector";
 
 const categories = [
   "친환경☘️",
@@ -19,11 +21,61 @@ const categories = [
   "AI/테크💻",
   "패션/뷰티💄",
 ];
-const flatforms = ["iOS", "Android", "Web"];
-const options = ["iOS", "Android", "Web"];
+const platforms = ["iOS", "Android", "Web"];
 
 export default function Page() {
   const router = useRouter();
+
+  // 상태 꺼내오는 코드들
+  const projectName = useProjectGenerateStore((state) => state.projectName);
+  const projectCategories = useProjectGenerateStore(
+    (state) => state.projectCategories,
+  );
+  const platform = useProjectGenerateStore((state) => state.platform);
+  const projectImage = useProjectGenerateStore((state) => state.projectImage);
+  const mustOffline = useProjectGenerateStore((state) => state.mustOffline);
+  const myField = useProjectGenerateStore((state) => state.myField);
+  const recruitField = useProjectGenerateStore((state) => state.recruitField);
+  const projectDeadline = useProjectGenerateStore(
+    (state) => state.projectDeadline,
+  );
+  const projectDescription = useProjectGenerateStore(
+    (state) => state.projectDescription,
+  );
+
+  // setter 함수들
+  const setProjectName = useProjectGenerateStore(
+    (state) => state.setProjectName,
+  );
+  const setProjectCategories = useProjectGenerateStore(
+    (state) => state.setProjectCategories,
+  );
+  const setPlatform = useProjectGenerateStore((state) => state.setPlatform);
+  const setProjectImage = useProjectGenerateStore(
+    (state) => state.setProjectImage,
+  );
+  const setMustOffline = useProjectGenerateStore(
+    (state) => state.setMustOffline,
+  );
+  const setMyField = useProjectGenerateStore((state) => state.setMyField);
+  const setRecruitField = useProjectGenerateStore(
+    (state) => state.setRecruitField,
+  );
+  const setProjectDeadline = useProjectGenerateStore(
+    (state) => state.setProjectDeadline,
+  );
+  const setProjectDescription = useProjectGenerateStore(
+    (state) => state.setProjectDescription,
+  );
+  const reset = useProjectGenerateStore((state) => state.reset);
+
+  function checkField() {}
+
+  useEffect(() => {
+    console.log(platform);
+    console.log(projectImage);
+    console.log(mustOffline);
+  }, [platform, projectCategories, projectImage, mustOffline]);
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -33,25 +85,30 @@ export default function Page() {
           <Input
             title="프로젝트 명"
             placeholder="프로젝트 이름을 입력해주세요"
+            value={projectName}
+            onChange={setProjectName}
           />
           <SelectableButtonGroup
             title={"프로젝트 카테고리"}
             subtitle="프로젝트 카테고리를 선택해주세요!"
             optionList={categories}
+            onChangeOptions={setProjectCategories}
           />
-          <SelectableButtonGroup title={"플랫폼"} optionList={flatforms} />
-          <ImageSelector />
-          <BinaryOptionSelector
+          <SelectableButtonGroup
+            title={"플랫폼"}
+            optionList={platforms}
+            onChangeOptions={setPlatform}
+          />
+          <ImageSelector value={projectImage} onChange={setProjectImage} />
+          <BinaryOptionSelector<"필수" | "선택">
             title={"오프라인 정기모임 필수 여부"}
             option1={"필수"}
             option2={"선택"}
+            onClickOption={setMustOffline}
           />
           <div className="flex flex-col gap-4 w-full">
             <b>나의 포지션</b>
-            <div className="w-full flex flex-1 flex-row gap-2">
-              <Selectable options={options} />
-              <Selectable options={options} />
-            </div>
+            <FieldSelector />
           </div>
           <Recruit />
           <TechSearch />

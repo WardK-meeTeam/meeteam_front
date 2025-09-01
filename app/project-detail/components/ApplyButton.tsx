fa79b12aa87427131e1f4db9a5fadc41bcfaa410
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-01 16:55:44 +0900

    [MTM-5] [#62] feat: 지원마감 상태 추가

--- app/project-detail/components/ApplyButton.tsx
+++ app/project-detail/components/ApplyButton.tsx
@@ -1,10 +1,14 @@
-export default function ApplyButton() {
+export default function ApplyButton({ disabled }: { disabled: boolean }) {
+  console.log(disabled);
   return (
     <button
-      className="flex justify-center items-center py-2 px-6 box-border bg-black text-white text-[14px] font-semibold w-[104px] h-[33px] rounded-sm
-    cursor-pointer"
+      disabled={disabled}
+      className={`flex justify-center items-center py-2 px-6 box-border
+        ${disabled ? "bg-mtm-light-gray text-mtm-text-gray" : "bg-black text-white cursor-pointer"}
+    text-[14px] font-semibold w-[104px] h-[33px] rounded-sm
+    `}
     >
-      지원하기
+      {disabled ? "지원마감" : "지원하기"}
     </button>
   );
 }

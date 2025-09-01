c79d377cea8f13493f47cef5dc642020983f31f4
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-01 19:47:42 +0900

    [MTM-5] [#62] feat: 팀 멤버 스크롤 따라오도록 변경

--- app/project-detail/[projectId]/page.tsx
+++ app/project-detail/[projectId]/page.tsx
@@ -10,14 +10,16 @@ export default async function Page({
   const { projectId } = await params;
   return (
     <div className="flex flex-row gap-16 max-w-7xl mx-auto pb-24">
-      <aside className="flex flex-col gap-3 w-[194px] flex-1">
-        <span className="text-[14px]">프로젝트를 함께할 사람들</span>
-        <MemberList />
+      <aside className="w-[194px] flex-1">
+        <div className="sticky top-10 z-10 flex flex-col gap-3">
+          <span className="text-[14px]">프로젝트를 함께할 사람들</span>
+          <MemberList />
+        </div>
       </aside>
       <main className="max-w-[830px] flex flex-col gap-16">
         <ProjectInfo />
         <hr className="text-mtm-light-gray" />
-        <ProjectRecruitInfo />
+        <ProjectRecruitInfo projectId={projectId} />
       </main>
     </div>
   );

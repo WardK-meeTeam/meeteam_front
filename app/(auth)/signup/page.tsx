40b586f17e1376be87e977146f9b74c1a202c583
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-04 20:42:18 +0900

    [#1] feat: 변경된 input 컴포넌트 Props로 재설정 및 소셜 로그인 버튼 추가

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { createAccount } from "./actions";
-import { useActionState } from "react";
+import { useActionState, useEffect } from "react";
 import { PASSWORD_MIN_LENGTH } from "@/app/lib/constants";
-import Input from "@/app/components/input";
-import SocialLogin from "@/app/components/social-login";
-import LongButton from "@/app/components/longButton";
 import React, { useState } from "react";
+import SocialSignInButton from "./components/SocialSignInButton";
+import MainButton from "@/components/MainButton";
+import Input from "@/components/Input";
 
 export default function CreateAccount() {
   const [state, action] = useActionState(createAccount, null);
@@ -28,17 +28,24 @@ export default function CreateAccount() {
   const isAnyEmpty = Object.values(form).some((v) => !v);
 
   return (
-    <div className="flex flex-col gap-10 py-8 px-6 items-center">
-      <div className="flex flex-col gap-2 *:font-medium">
-        <h1 className="text-2xl">meeTeam</h1>
+    <div className="w-[340px] m-auto box-border justify-start items-center flex flex-col flex-1 gap-8">
+      <h1 className="text-2xl font-bold text-mtm-main-blue">meeTeam</h1>
+      <div className="flex flex-col w-full justify-start items-center gap-3 ">
+        <SocialSignInButton platform="google" />
+        <SocialSignInButton platform="github" text="Github로 로그인하기" />
       </div>
-      <SocialLogin />
-      <div className="w-96 h-px bg-neutral-500" />
-      <form action={action} className="flex flex-col gap-3">
+
+      <span className="flex flex-row justify-center items-center w-full box-border">
+        <div className="bg-mtm-light-gray h-px flex-1" />
+        <span className="px-4 text-xs">or</span>
+        <div className="bg-mtm-light-gray h-px flex-1" />
+      </span>
+      <form action={action} className="flex flex-col gap-3 w-full">
         <span className="text-md -mb-2">이름</span>
         <Input
           name="username"
           type="text"
+          value={form.username}
           required={true}
           errors={state?.fieldErrors?.username}
           onChange={handleChange}
@@ -47,6 +54,7 @@ export default function CreateAccount() {
         <Input
           name="email"
           type="email"
+          value={form.email}
           required={true}
           errors={state?.fieldErrors?.email}
           onChange={handleChange}
@@ -55,6 +63,7 @@ export default function CreateAccount() {
         <Input
           name="password"
           type="password"
+          value={form.password}
           required={true}
           errors={state?.fieldErrors?.password}
           minLength={PASSWORD_MIN_LENGTH}
@@ -64,12 +73,13 @@ export default function CreateAccount() {
         <Input
           name="confirm_password"
           type="password"
+          value={form.confirm_password}
           required={true}
           errors={state?.fieldErrors?.confirm_password}
           minLength={PASSWORD_MIN_LENGTH}
           onChange={handleChange}
         />
-        <LongButton text="확인" marginTop="mt-10" disabled={isAnyEmpty} />
+        <MainButton buttonName="확인" disabled={isAnyEmpty} />
       </form>
     </div>
   );

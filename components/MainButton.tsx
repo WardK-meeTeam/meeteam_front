db51c846ca188f2ae4b32253b09b49fa0d61efa3
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-15 14:30:33 +0900

    [MTM-7] [#64] chore: MainButton type Props 추가

--- components/MainButton.tsx
+++ components/MainButton.tsx
@@ -3,6 +3,8 @@ export interface MainButtonProps {
   disabled: boolean;
   width?: number;
   height?: number;
+  type: "button" | "submit" | "reset" | undefined;
+
   onClick?: () => void;
 }
 
@@ -10,11 +12,13 @@ export default function MainButton({
   buttonName,
   disabled,
   onClick,
+  type = "button",
 }: MainButtonProps) {
   return (
     <button
       disabled={disabled}
       onClick={onClick}
+      type={type}
       className={`rounded-[10px] border font-bold py-3 px-8 text-white ${disabled ? `bg-mtm-button-disabled border-mtm-button-disabled` : `bg-mtm-main-blue border-mtm-main-blue cursor-pointer`}`}
     >
       {buttonName}

1a66db2dbaacca9c2a0839c61f530b580e97870b
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-15 11:07:29 +0900

    [#1] chore: 에러메세지 한글화

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,6 +1,6 @@
 export const PASSWORD_MIN_LENGTH = 4;
 export const PASSWORD_REGEX = new RegExp(
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#?!@$%^&*-]).+$/
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#?!@$%^&*-]).+$/,
 );
 export const PASSWORD_REGEX_ERROR =
-  "Password too weak, must contain at least one uppercase letter, one lowercase letter, and one special character";
+  "비밀번호는 대문자, 소문자, 특수문자를 각각 최소 1개 이상 포함해야 합니다.";

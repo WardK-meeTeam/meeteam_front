58c6995c8b361fbd9ea5d26af46a56edcc843ad1
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-16 18:48:54 +0900

    [MTM-11] [#77] feat: 이메일,비번,비번확인 입력란 setting-after-signup페이지로 이동

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,29 +1,31 @@
 "use client";
 
-import { createAccount } from "./actions";
-import { useActionState, useState } from "react";
-import { PASSWORD_MIN_LENGTH } from "@/app/lib/constants";
+// import { createAccount } from "./actions";
+// import { useActionState, useState } from "react";
+// import { PASSWORD_MIN_LENGTH } from "@/app/lib/constants";
 import SocialSignInButton from "./components/SocialSignInButton";
 import MainButton from "@/components/MainButton";
-import Input from "@/components/Input";
+// import Input from "@/components/Input";
+import { useRouter } from "next/navigation";
 
 export default function CreateAccount() {
-  const [state, action, pending] = useActionState(createAccount, null);
+  // const [state, action, pending] = useActionState(createAccount, null);
+  const router = useRouter();
 
   // 입력값 상태 추가
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-    confirm_password: "",
-  });
+  // const [form, setForm] = useState({
+  //   email: "",
+  //   password: "",
+  //   confirm_password: "",
+  // });
 
   // 입력값 변경 핸들러
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
+  // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setForm({ ...form, [e.target.name]: e.target.value });
+  // };
 
   // 하나라도 비어 있으면 true
-  const isAnyEmpty = Object.values(form).some((v) => !v);
+  // const isAnyEmpty = Object.values(form).some((v) => !v);
 
   const API = process.env.NEXT_PUBLIC_API_BASE_URL;
 
@@ -51,49 +53,12 @@ export default function CreateAccount() {
         <span className="px-4 text-xs">or</span>
         <div className="bg-mtm-light-gray h-px flex-1" />
       </span>
-      <form action={action} className="flex flex-col gap-3 w-full">
-        <div className="flex flex-col">
-          <span className="text-md">Email</span>
-          <Input
-            name="email"
-            type="email"
-            value={form.email}
-            required={true}
-            errors={state?.fieldErrors?.email}
-            onChange={handleChange}
-          />
-        </div>
 
-        <div className="flex flex-col">
-          <span className="text-md">비밀번호</span>
-          <Input
-            name="password"
-            type="password"
-            value={form.password}
-            required={true}
-            errors={state?.fieldErrors?.password}
-            minLength={PASSWORD_MIN_LENGTH}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <span className="text-md">비밀번호 확인</span>
-          <Input
-            name="confirm_password"
-            type="password"
-            value={form.confirm_password}
-            required={true}
-            errors={state?.fieldErrors?.confirm_password}
-            minLength={PASSWORD_MIN_LENGTH}
-            onChange={handleChange}
-          />
-        </div>
-        <MainButton
-          buttonName={pending ? "처리 중..." : "확인"}
-          disabled={isAnyEmpty || pending}
-        />
-      </form>
+      <MainButton
+        buttonName="이메일로 시작하기"
+        disabled={false}
+        onClick={() => router.push("/setting-after-signup")}
+      />
     </div>
   );
 }

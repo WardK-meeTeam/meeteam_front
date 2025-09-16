e793a29cff3c8166bad1ad173fae3e8f1a0333d3
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-16 18:47:51 +0900

    [MTM-11] [#77] feat: 이메일,비밀번호,비밀번호 확인 state 추가

--- store/signupDataStore.ts
+++ store/signupDataStore.ts
@@ -4,17 +4,23 @@ import { recruitFieldItem } from "./projectGenerateStore";
 export interface userFieldItem extends Pick<recruitFieldItem, "id" | "field"> {}
 
 export interface SignUpState {
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
   userName: string;
   birthDate: string | null;
-  gender: "여성" | "남성";
+  gender: "MALE" | "FEMALE";
   field: userFieldItem[];
   skills: string[];
   profileImg: string | null;
   introduction: string;
 
+  setEmail: (email: string) => void;
+  setPassword: (password: string) => void;
+  setConfirmPassword: (password: string) => void;
   setUserName: (un: string) => void;
   setBirthDate: (bd: string | null) => void;
-  setGender: (g: "여성" | "남성") => void;
+  setGender: (g: "MALE" | "FEMALE") => void;
   setField: (f: userFieldItem[]) => void;
   setSkills: (sk: string[]) => void;
   setProfileImg: (img: string | null) => void;
@@ -23,15 +29,21 @@ export interface SignUpState {
 }
 
 export const useSignUpStore = create<SignUpState>((set) => ({
+  email: "",
+  password: "",
+  confirmPassword: "",
   userName: "",
   birthDate: "",
-  gender: "남성",
+  gender: "MALE",
   field: [{ id: 0, field: null }],
   skills: [],
   profileImg: null,
   introduction: "",
 
   // setter 함수 모음
+  setEmail: (email) => set({ email }),
+  setPassword: (password) => set({ password }),
+  setConfirmPassword: (confirmPassword) => set({ confirmPassword }),
   setUserName: (un) => set({ userName: un }),
   setBirthDate: (bd) => set({ birthDate: bd }),
   setGender: (g) => set({ gender: g }),
@@ -43,9 +55,12 @@ export const useSignUpStore = create<SignUpState>((set) => ({
   // reset 함수
   reset: () =>
     set({
+      email: "",
+      password: "",
+      confirmPassword: "",
       userName: "",
       birthDate: "",
-      gender: "남성",
+      gender: "MALE",
       field: [{ id: 0, field: null }],
       skills: [],
       profileImg: null,

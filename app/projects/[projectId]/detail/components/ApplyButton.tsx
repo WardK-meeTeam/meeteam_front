6ad131ee1f467fc3bdccabe60b39e7d28f05d3b4
unknown <jhje5595@naver.com>
2025-09-18 01:03:30 +0900

    [MTM-13] [#79] chore: 클릭시 해당 프로젝트의 해당 모집분야 지원폼으로 이동하도록 수정

--- app/projects/[projectId]/detail/components/ApplyButton.tsx
+++ app/projects/[projectId]/detail/components/ApplyButton.tsx
@@ -1,7 +1,26 @@
-export default function ApplyButton({ disabled }: { disabled: boolean }) {
+import { convertSubCategory } from "@/utils/subCategoryMap";
+import { useRouter } from "next/navigation";
+
+interface ApplyButtonProps {
+  disabled: boolean;
+  projectId: string;
+  subCategory: string;
+}
+
+export default function ApplyButton({
+  disabled,
+  projectId,
+  subCategory,
+}: ApplyButtonProps) {
+  const router = useRouter();
   return (
     <button
       disabled={disabled}
+      onClick={() =>
+        router.push(
+          `/projects/${projectId}/apply?category=${convertSubCategory(subCategory)}`,
+        )
+      }
       className={`flex justify-center items-center py-2 px-6 box-border
         ${disabled ? "bg-mtm-light-gray text-mtm-text-gray" : "bg-black text-white cursor-pointer"}
     text-[14px] font-semibold w-[104px] h-[33px] rounded-sm

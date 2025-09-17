e8c3682d8b402e4e0a6920e697d4468d24fc5e05
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-17 16:57:50 +0900

    [MTM-12] feat: 중복 선택 불가능 옵션 추가

--- components/SelectableButtonGroup.tsx
+++ components/SelectableButtonGroup.tsx
@@ -3,18 +3,25 @@ import ToggleButton from "@/components/ToggleButton";
 
 export default function SelectableButtonGroup({
   title,
-  subtitle,
+  errors,
   optionList,
   value,
   onChange,
+  onlySelectOne = false,
 }: {
   title: string;
-  subtitle?: string;
+  errors?: string;
   optionList: string[];
   value: string[];
   onChange: (selected: string[]) => void;
+  onlySelectOne?: boolean;
 }) {
   function handleClickPartButton(part: string) {
+    if (onlySelectOne) {
+      // 그룹 중에서 1개만 선택되어야 하기 떄문에 그냥 기존거 무시하고 자기만 들어가면 됨
+      onChange([part]);
+      return;
+    }
     const isSelected = value.includes(part);
     if (isSelected) {
       onChange(value.filter((item) => item !== part));
@@ -27,7 +34,7 @@ export default function SelectableButtonGroup({
     <div className="flex flex-col gap-4">
       <div className="flex flex-row gap-2 items-center">
         <b>{title}</b>
-        <span className="text-red-500 text-xs">{subtitle ? subtitle : ""}</span>
+        <span className="text-red-500 text-xs">{errors ? errors : ""}</span>
       </div>
       <div className="flex flex-row flex-wrap w-full gap-3">
         {optionList.map((part) => (

4bb1a4378d16a6c8c83c8320eccd4ac5b07a4b7c
unknown <jhje5595@naver.com>
2025-09-17 23:00:24 +0900

    [MTM-13] [#79] feat: 프로젝트 타입 별도 분리

--- app/project-detail/[projectId]/ProjectPageClient.tsx
+++ app/project-detail/[projectId]/ProjectPageClient.tsx
@@ -1,15 +1,37 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import MemberList from "../components/MemberList";
 import ProjectInfo from "../components/ProjectInfo";
 import ProjectRecruitInfo from "../components/ProjectRecruitInfo";
+import {
+  Member,
+  ProjectInfoItem,
+  ProjectRecruitInfoItem,
+} from "@/types/projectInfo";
 
 export default function ProjectPageClient({
   projectId,
 }: {
   projectId: string;
 }) {
+  const [projectMembers, setProjectMembers] = useState<Member[]>([]);
+  const [projectInfo, setProjectInfo] = useState<ProjectInfoItem>({
+    name: "",
+    description: "",
+    likeCount: 0,
+    platformCategory: "",
+    projectCategory: "",
+    imageUrl: null,
+    startDate: "",
+  });
+  const [projectRecruit, setProjectRecruit] = useState<ProjectRecruitInfoItem>({
+    offlineRequired: false,
+    endDate: "",
+    skills: [],
+    recruitments: [],
+  });
+
   const API = process.env.NEXT_PUBLIC_API_BASE_URL;
 
   const fetchProjectDetail = async () => {
@@ -22,27 +44,50 @@ export default function ProjectPageClient({
     });
 
     if (response.ok) {
-      console.log("굿", await response.json());
+      const data = await response.json();
+
+      const receivedProjectInfo = {
+        name: data.result.name,
+        description: data.result.description,
+        likeCount: data.result.likeCount,
+        platformCategory: data.result.platformCategory,
+        projectCategory: data.result.projectCategory,
+        imageUrl: data.result.imageUrl,
+        startDate: data.result.startDate,
+      };
+
+      const receivedProjectRecruitInfo = {
+        offlineRequired: data.result.offlineRequired,
+        endDate: data.result.endDate,
+        skills: data.result.skills,
+        recruitments: data.result.recruitments,
+      };
+
+      setProjectMembers(data.result.projectMembers);
+      setProjectInfo(receivedProjectInfo);
+      setProjectRecruit(receivedProjectRecruitInfo);
     } else {
-      console.log(await response.json());
+      const errorData = await response.json();
+      alert(errorData.message);
     }
   };
 
   useEffect(() => {
     fetchProjectDetail();
   }, [projectId]);
+
   return (
     <div className="flex flex-row gap-16 max-w-7xl mx-auto pb-24">
       <aside className="w-[194px] flex-1">
         <div className="sticky top-20 z-10 flex flex-col gap-3">
           <span className="text-[14px]">프로젝트를 함께할 사람들</span>
-          <MemberList />
+          <MemberList members={projectMembers} />
         </div>
       </aside>
       <main className="max-w-[830px] flex flex-col gap-16">
-        <ProjectInfo projectId={projectId} />
+        <ProjectInfo projectId={projectId} {...projectInfo} />
         <hr className="text-mtm-light-gray" />
-        <ProjectRecruitInfo projectId={projectId} />
+        <ProjectRecruitInfo projectId={projectId} {...projectRecruit} />
       </main>
     </div>
   );

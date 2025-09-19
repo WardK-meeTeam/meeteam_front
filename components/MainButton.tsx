e31a6e0a57c6fb396d093eb61ffbabcb0983d285
unknown <jhje5595@naver.com>
2025-09-20 02:49:17 +0900

    [MTM-16] [#88] feat: 색상 반전 옵션 추가

--- components/MainButton.tsx
+++ components/MainButton.tsx
@@ -4,6 +4,7 @@ export interface MainButtonProps {
   width?: number;
   height?: number;
   type?: "button" | "submit" | "reset" | undefined;
+  invertedColor?: boolean;
 
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
@@ -13,13 +14,20 @@ export default function MainButton({
   disabled,
   onClick,
   type = "button",
+  invertedColor = false,
 }: MainButtonProps) {
   return (
     <button
       disabled={disabled}
       onClick={onClick}
       type={type}
-      className={`rounded-[10px] border font-bold py-3 px-8 text-white ${disabled ? `bg-mtm-button-disabled border-mtm-button-disabled` : `bg-mtm-main-blue border-mtm-main-blue cursor-pointer`}`}
+      className={`rounded-[10px] border font-bold py-3 px-8 ${
+        disabled
+          ? `bg-mtm-button-disabled border-mtm-button-disabled text-white`
+          : invertedColor
+            ? "bg-mtm-light-blue border-mtm-light-blue cursor-pointer text-mtm-main-blue"
+            : `bg-mtm-main-blue border-mtm-main-blue cursor-pointer text-white`
+      }`}
     >
       {buttonName}
     </button>

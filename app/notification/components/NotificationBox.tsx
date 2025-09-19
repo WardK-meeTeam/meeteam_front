d8fb18f8ed562f6f039513f826342bcc0a6506a4
정연준 <yeonjun@jeong-yeonjun-ui-MacBookAir.local>
2025-09-20 00:54:35 +0900

    [MTM-16] [#88] feat: 알림 컴포넌트 생성

---
+++ app/notification/components/NotificationBox.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Image from "next/image";
+import DotImg from "@/public/images/Ellipse.svg";
+import Link from "next/link";
+
+export interface Notification {
+  title: string;
+  description: string;
+  time: string;
+  buttonName?: string;
+}
+
+export default function NotificationBox({
+  title,
+  description,
+  time,
+  buttonName,
+}: Notification) {
+  return (
+    <div className="flex flex-col gap-3 border border-mtm-light-gray p-8 rounded-lg w-full">
+      <div className="flex flex-row items-center justify-start gap-1">
+        <h1 className="text-xl font-bold">{title}</h1>
+        <Image src={DotImg} width={2} height={2} alt="-" />
+        <span>{time}</span>
+      </div>
+
+      <div className="flex flex-row justify-between">
+        <span>{description}</span>
+        {buttonName && (
+          <Link
+            href={"/application?applicationId=2"}
+            className="cursor-pointer text-mtm-purple py-2 px-3 bg-mtm-light-purple
+          rounded-lg"
+          >
+            {buttonName}
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+}

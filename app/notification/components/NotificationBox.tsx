92aa3c0c63fcb6e803226ff32a4d1282f1b0ad4c
unknown <jhje5595@naver.com>
2025-09-21 01:28:02 +0900

    [MTM-16] [#88] feat: DB에서 가져온 data로 변경

--- app/notification/components/NotificationBox.tsx
+++ app/notification/components/NotificationBox.tsx
@@ -3,37 +3,42 @@
 import Image from "next/image";
 import DotImg from "@/public/images/Ellipse.svg";
 import Link from "next/link";
-
-export interface Notification {
-  title: string;
-  description: string;
-  time: string;
-  buttonName?: string;
-}
+import { Notification } from "@/types/notification";
 
 export default function NotificationBox({
-  title,
-  description,
-  time,
-  buttonName,
+  applicationId,
+  createdAt,
+  id,
+  message,
+  payload,
+  read,
+  type,
 }: Notification) {
   return (
     <div className="flex flex-col gap-3 border border-mtm-light-gray p-8 rounded-lg w-full">
       <div className="flex flex-row items-center justify-start gap-1">
-        <h1 className="text-xl font-bold">{title}</h1>
+        <h1 className="text-xl font-bold">{payload.projectName}</h1>
         <Image src={DotImg} width={2} height={2} alt="-" />
-        <span>{time}</span>
+        <span>{createdAt}</span>
+        {!read && <span className="h-2 w-2 bg-red-500 rounded-full"></span>}
       </div>
 
       <div className="flex flex-row justify-between">
-        <span>{description}</span>
-        {buttonName && (
+        <span>
+          {type === "PROJECT_MY_APPLY"
+            ? "✉️ "
+            : type === "PROJECT_APPLY"
+              ? "🙋‍♂️ "
+              : " "}
+          {message}
+        </span>
+        {type === "PROJECT_APPLY" && (
           <Link
-            href={"/application?applicationId=14&projectId=90"}
+            href={`/application?applicationId=${applicationId}&projectId=${payload.projectId}`}
             className="cursor-pointer text-mtm-purple py-2 px-3 bg-mtm-light-purple
           rounded-lg"
           >
-            {buttonName}
+            지원서 보기
           </Link>
         )}
       </div>

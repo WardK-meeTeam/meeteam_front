90df4a3bc8c4a65db8843506f50e4943c186d839
jebiyeon02 <jhje5595@naver.com>
2025-09-22 18:17:18 +0900

    [MTM-20] [#96] feat: 수정 페이지 row 생성

---
+++ app/users/edit/components/KeyValueRow.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+
+export default function KeyValueRow({
+  title,
+  value,
+}: {
+  title: string;
+  value: ReactNode;
+}) {
+  return (
+    <div className="flex flex-row gap-x-16 text-black justify-start items-baseline">
+      <span className="min-w-48 font-bold text-xl">{title}</span>
+      <span className="flex-1 ">{value} </span>
+    </div>
+  );
+}

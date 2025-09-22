e584eb1a6d730542daa6bc06f4360cd8464bc72c
jebiyeon02 <jhje5595@naver.com>
2025-09-22 21:36:53 +0900

    feat: 내정보를 localStorage가아닌 API fetch를 이용해 가져오도록 변경

--- app/projects/[projectId]/apply/components/UserProfileSummary.tsx
+++ app/projects/[projectId]/apply/components/UserProfileSummary.tsx
@@ -1,16 +1,23 @@
 "use client";
 
-import { UserProfile } from "@/types/userProfile";
-import { Fragment, useEffect, useState } from "react";
+import { useUserStore } from "@/store/userStore";
+import { Fragment, useEffect } from "react";
 
 export default function UserProfileSummary() {
-  const [user, setUser] = useState<UserProfile | null>(null);
+  const { user, isLoading, error, fetchUser } = useUserStore();
 
   useEffect(() => {
-    const savedUser = localStorage.getItem("user-storage");
-    if (!savedUser) return;
-    setUser(JSON.parse(savedUser).state.user);
-  }, []);
+    fetchUser();
+  }, [fetchUser]);
+
+  if (isLoading) {
+    return <div>로딩중...</div>;
+  }
+
+  if (error) {
+    return <div>에러 : {error}</div>;
+  }
+
   return (
     <div className="flex flex-col gap-5">
       <span className="flex justify-between items-center ">
@@ -18,29 +25,30 @@ export default function UserProfileSummary() {
         {/* <ModifyButton /> */}
       </span>
       <hr className="border-4 border-[#F8F8F8]" />
-      <div className="flex flex-col gap-6">
-        <span>
-          <b className="mr-3">이름</b> {user?.name ?? "-"}
-        </span>
-        <span>
-          <b className="mr-3">나이</b> {user?.age ?? "-"}세
-          <b className="ml-4 mr-3">성별</b>
-          {user?.gender === "MALE" ? "남성" : "여성"}
-        </span>
-        <span>
-          <b className="mr-3">이메일</b> {user?.email ?? "-"}
-        </span>
-        <span className="flex flex-row items-center justify-start">
-          <h1 className="mr-3 font-semibold">기술스택</h1>
-          <div>
-            {user &&
-              user.skills.map((item, idx) => (
-                <Fragment key={`${user.name}-${item.skill}`}>
-                  {`${item.skill}${idx !== user.skills.length - 1 ? "," : ""}`}
-                </Fragment>
-              ))}
-          </div>
-        </span>
+      <div className="grid grid-cols-[80px_1fr] gap-y-4 gap-x-5">
+        <b className="font-bold">이름</b>
+        <span>{user?.name ?? "-"}</span>
+
+        <b className="font-bold">나이</b>
+        <div>
+          <span>{user?.age ?? "-"}세</span>
+        </div>
+
+        <b className="font-bold">성별</b>
+        <span>{user?.gender === "MALE" ? "남성" : "여성"}</span>
+
+        <b className="font-bold">이메일</b>
+        <span>{user?.email ?? "-"}</span>
+
+        <b className="font-bold">기술스택</b>
+        <div>
+          {user &&
+            user.skills.map((item, idx) => (
+              <Fragment key={`${user.name}-${item.skill}`}>
+                {`${item.skill}${idx !== user.skills.length - 1 ? "," : ""}`}
+              </Fragment>
+            ))}
+        </div>
       </div>
     </div>
   );

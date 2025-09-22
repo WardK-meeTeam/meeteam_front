2d6b615039ab29ec6c2d3a633e5eacc640d1a4ea
jebiyeon02 <jhje5595@naver.com>
2025-09-22 18:17:06 +0900

    [MTM-20] [#96] chore: span -> Link태그로 변경

--- app/projects/[projectId]/apply/components/ModifyButton.tsx
+++ app/projects/[projectId]/apply/components/ModifyButton.tsx
@@ -1,16 +1,17 @@
 "use client";
-
+``;
 import Image from "next/image";
 import ArrowIcon from "@/public/images/right_arrow_icon.svg";
+import Link from "next/link";
 
 export default function ModifyButton() {
   return (
-    <span
-      onClick={() => alert("기본정보 수정 모달 띄우기")}
-      className="text-[#AD5FFF] flex gap-2 cursor-pointer text-[14px]"
+    <Link
+      href={"/users/edit"}
+      className="text-mtm-purple flex gap-2 cursor-pointer text-[14px]"
     >
-      수정하기
+      정보 수정하기
       <Image src={ArrowIcon} alt="수정하기" width={8} height={12} />
-    </span>
+    </Link>
   );
 }

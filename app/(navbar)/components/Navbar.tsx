ecfdca52662e6f40c5f24b0fb307a8133374dd42
unknown <jhje5595@naver.com>
2025-09-24 02:03:18 +0900

    chore: useRouter import 제거

--- app/(navbar)/components/Navbar.tsx
+++ app/(navbar)/components/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { Fragment, useEffect, useState } from "react";
 import Bell from "@/public/images/Bell.svg";
 import { NotificationToast } from "../../../components/NotificationToast";

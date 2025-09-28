c0e0985cbb252aa324ec91d98ec87430ed99cc8b
unknown <jhje5595@naver.com>
2025-09-28 16:29:12 +0900

    style: 이름,안녕하세요 사이 띄어쓰기

--- app/(navbar)/components/Navbar.tsx
+++ app/(navbar)/components/Navbar.tsx
@@ -128,7 +128,7 @@ export default function Navbar() {
             <li className="text-mtm-text-gray pl-2 min-w-50">
               <Link href={`/users/${user.memberId}`} className="font-bold">
                 {user?.name ?? ""}님!
-              </Link>
+              </Link>{" "}
               안녕하세요
             </li>
           ) : (

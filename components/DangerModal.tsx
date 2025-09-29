01497bd5fe41f6d4280c6ade6badeb184decaf8d
jebiyeon02 <jhje5595@naver.com>
2025-09-29 11:25:23 +0900

    [MTM-23] [#101] feat: 삭제 확인 컴포넌트 생성

--- components/DangerModal.tsx
+++ components/DangerModal.tsx
@@ -1,4 +1,5 @@
-import { Dispatch, SetStateAction } from "react";
+"use client";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import Modal from "./Modal";
 
 interface DangerModalProps {
@@ -6,42 +7,58 @@ interface DangerModalProps {
   description?: string;
   isOpen: boolean;
   onChangeOpen: Dispatch<SetStateAction<boolean>>;
-  disabled?: boolean;
   onClickButton?: () => void;
+  isLoading?: boolean;
+  cancelText: string;
 }
 
 export default function DangerModal({
   isOpen = false,
   onChangeOpen,
-  disabled = false,
   onClickButton = () => {},
   title,
   description,
+  isLoading = false,
+  cancelText,
 }: DangerModalProps) {
+  const [text, setText] = useState("");
+  const [correctInput, setCorrectInput] = useState(false);
+
+  useEffect(() => {
+    if (text.trim() === "삭제하기") setCorrectInput(true);
+    else setCorrectInput(false);
+  }, [text]);
   return (
     <>
       {isOpen && (
         <Modal>
-          <div className="flex flex-col justify-start items-center gap-4 max-w-2xs mx-auto">
-            <h3 className="font-medium">{title ?? ""}</h3>
-            <h4 className="text-xs">{description}</h4>
+          <div className="flex flex-col justify-start items-center gap-4 min-w-2xs mx-auto">
+            <div className="flex flex-col justify-start items-center">
+              <h3 className="font-medium">{title ?? ""}</h3>
+              <h4 className="text-xs">{description}</h4>
+            </div>
             <input
               placeholder="삭제하기"
-              className="w-full py-3 px-5 rounded-xl text-[14px]"
+              className="w-full py-3 px-5 rounded-xl text-[14px] border border-mtm-light-gray"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
             />
-            <div className="flex flex-col justify-end gap-4 mt-4">
-              <button
+            <div className="flex flex-col justify-start gap-4 w-full">
+              <span
                 onClick={() => onChangeOpen(false)}
-                className="px-4 py-2 bg-gray-200 rounded-md"
+                className="underline cursor-pointer self-center"
               >
-                취소
-              </button>
+                {cancelText}
+              </span>
               <button
                 onClick={onClickButton}
-                disabled={disabled}
-                className="px-4 py-2 bg-red-500 text-white rounded-md disabled:bg-gray-400"
+                disabled={!correctInput || isLoading}
+                type="button"
+                className={`px-4 py-2 bg-mtm-main-red text-white rounded-md disabled:bg-gray-400
+                  ${!correctInput || isLoading ? "" : "cursor-pointer`"}
+                  `}
               >
-                {disabled ? "삭제 중..." : "삭제"}
+                {isLoading ? "삭제 중..." : "삭제하기"}
               </button>
             </div>
           </div>

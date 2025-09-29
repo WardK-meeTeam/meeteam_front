b408ae604e2546c4ed74687ca8a699356fbae489
jebiyeon02 <jhje5595@naver.com>
2025-09-29 09:59:10 +0900

    [MTM-23] [#101] style: 너비 100% 설정

--- app/(navbar)/projects/[projectId]/components/MemberList.tsx
+++ app/(navbar)/projects/[projectId]/components/MemberList.tsx
@@ -7,7 +7,7 @@ export default function MemberList({ members }: { members: Member[] }) {
   const leaders = members.filter((member) => member.creator === true);
   const teamMembers = members.filter((member) => member.creator !== true);
   return (
-    <div className="flex flex-col gap-8 box-border border border-mtm-light-gray py-5 px-7 rounded-xl">
+    <div className="flex flex-col gap-8 box-border border border-mtm-light-gray py-5 px-7 rounded-xl w-full">
       <div className="flex flex-col gap-3">
         <span className="font-bold text-lg">팀장</span>
         {leaders.map((person, idx) => (

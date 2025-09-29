077e93672213763bb33a85b399ecf6b62f23933c
jebiyeon02 <jhje5595@naver.com>
2025-09-29 14:21:34 +0900

    [MTM-23] [#101] chore: MemberList에 projectId 넘겨줌

--- app/(navbar)/projects/[projectId]/detail/ProjectPageClient.tsx
+++ app/(navbar)/projects/[projectId]/detail/ProjectPageClient.tsx
@@ -78,7 +78,7 @@ export default function ProjectPageClient({
       <aside className="w-[194px] flex-1">
         <div className="sticky top-20 z-10 flex flex-col gap-3">
           <span className="text-[14px]">프로젝트를 함께할 사람들</span>
-          <MemberList members={projectMembers} />
+          <MemberList members={projectMembers} projectId={projectId} />
           <button
             type="button"
             onClick={() => router.push(`/projects/${projectId}/manage`)}

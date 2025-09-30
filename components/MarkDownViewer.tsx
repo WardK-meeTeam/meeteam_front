9dd79e4930ba3290a2ab91366db3643a5af76339
jebiyeon02 <jhje5595@naver.com>
2025-09-30 20:35:48 +0900

    [MTM-33] [#120] feat: 마크다운 배경색 제거 및 textColor props 생성

--- components/MarkDownViewer.tsx
+++ components/MarkDownViewer.tsx
@@ -2,12 +2,17 @@ import MDEditor from "@uiw/react-md-editor";
 
 export default function MarkDownViewer({
   markDownText,
+  textColor = "black",
 }: {
   markDownText: string;
+  textColor?: string;
 }) {
   return (
     <div data-color-mode="light" className="w-full">
-      <MDEditor.Markdown source={markDownText} />
+      <MDEditor.Markdown
+        source={markDownText}
+        style={{ backgroundColor: "transparent", color: textColor }}
+      />
     </div>
   );
 }

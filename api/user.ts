b50a07093f9028b1832c614f6f8a2db293ed9dc7
Yoonhyoyeon <30565205+yoonhyoyeon@users.noreply.github.com>
2025-10-02 19:23:17 +0900

    [MTM-32] [#118] fix: 서버 사이드에서는 토큰 갱신 후 재요청만 (저장은 클라이언트 사이드 요청들어오면)

--- api/user.ts
+++ api/user.ts
@@ -1,12 +1,17 @@
 import { authFetch } from "./authFetch";
 
 export const getUserProfile = async () => {
-  const response = await authFetch(`/api/members`);
+  try {
+    const response = await authFetch(`/api/members`);
 
-  if (!response.ok) {
+    if (!response.ok) {
+      return null;
+    }
+    const data = await response.json();
+
+    return data.result;
+  } catch (error) {
+    console.error("유저 정보 조회 실패", error);
     return null;
   }
-  const data = await response.json();
-
-  return data.result;
 };
